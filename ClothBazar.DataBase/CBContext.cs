744b878cd0687c9d1f0904a93caa110a0208c5d0
Abbas263 <Abbas.ali5788@gmail.com>
2018-12-18 00:25:59 -0800

    migration performed and Database created

---
+++ ClothBazar.DataBase/CBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using ClothBazar.Entities;
+
+namespace ClothBazar.DataBase
+{
+    class CBContext:DbContext
+    {
+        public CBContext(): base("ClothBazarConnection")
+        {
+                
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
